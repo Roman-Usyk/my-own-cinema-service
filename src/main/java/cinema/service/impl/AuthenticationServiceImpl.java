262a15c5e426430120775668ca23b334739bd9aa
Roman Usyk <roman.usik.93@ukr.net>
2023-03-24 14:19:02 +0200

    project is ready

---
+++ src/main/java/cinema/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,39 @@
+package cinema.service.impl;
+
+import cinema.model.Role;
+import cinema.model.User;
+import cinema.service.AuthenticationService;
+import cinema.service.RoleService;
+import cinema.service.ShoppingCartService;
+import cinema.service.UserService;
+import java.util.Set;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final String USER_ROLE = Role.RoleName.USER.name();
+    private final UserService userService;
+    private final RoleService roleService;
+    private final ShoppingCartService shoppingCartService;
+
+    public AuthenticationServiceImpl(UserService userService,
+                                     RoleService roleService,
+                                     ShoppingCartService shoppingCartService) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.shoppingCartService = shoppingCartService;
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        Role role = roleService.getByName(USER_ROLE);
+        user.setRoles(Set.of(role));
+        user.setRoles(Set.of());
+        userService.add(user);
+        shoppingCartService.registerNewShoppingCart(user);
+        return user;
+    }
+}
